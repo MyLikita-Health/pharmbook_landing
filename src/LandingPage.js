9bf3b6352c38d668477619181e31805c75b8acc0
ahmad kabir <atomakabir001@gmail.com>
2022-11-03 16:26:31 +0100

    save

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,6 +1,9 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
 import { Col, Row } from 'reactstrap'
+import { CiSettings } from 'react-icons/ci'
+import { TbLockAccess } from 'react-icons/tb'
+import { SlSupport } from 'react-icons/sl'
 
 export default function LandingPage() {
   return (
@@ -17,7 +20,7 @@ export default function LandingPage() {
                 <Row className='p-0 m-0'>
                     <Col md={1}></Col>
                     <Col md={5}>
-                        <img src={require('./IMG-20221030-WA0006.jpg')} alt='Logo' style={{ width: 200, marginTop: '28%' }}/>
+                        {/* <img src={require('./IMG-20221030-WA0006.jpg')} alt='Logo' style={{ width: 200, marginTop: '28%' }}/> */}
                         <h1 className='text-white title'>PharmBook</h1>
                         <p className='Discribtion'>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
                         Rerum rem soluta sit eius necessitatibus voluptate excepturi beatae ad 
@@ -62,7 +65,7 @@ export default function LandingPage() {
                         <center>
                             <div style={{marginTop: '4%'}}>
                                 <h5 className='text-white' style={{fontSize: '15px',color: '#cccccc'}}>Why Choose Us</h5>
-                                <h1 className='text-white'> Easy Management for Your Businesses </h1>
+                                <h1 className='text-white '> Easy Management for Your Businesses </h1>
                                 <p style={{color: '#f2f2f2'}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
                                    Rerum rem soluta sit eius necessitatibus voluptate excepturi beatae ad 
                                    eveniet sapiente impedit quae modi quo provident odit molestias! Rem reprehenderit assumenda
@@ -70,13 +73,86 @@ export default function LandingPage() {
                             </div>
                             <div style={{display: "flex",flexDirection: "row"}}>
                                 <div className='detail'>
-                                    {/* ahmad */}
+                                    <center><span ><CiSettings className= 'circle' style={{fontSize: 30,marginTop: 20}}/></span></center>
+                                    <center><h4 className='mt-3 details'>Easy Management</h4></center> 
+                                    <center><p className='more_info'>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                                   Rerum rem soluta sit eius necessitatibus voluptate excepturi beatae ad 
+                                   eveniet sapiente impedit quae modi quo provident odit molestias! Rem reprehenderit assumenda</p></center> 
                                 </div>
                                 <div className='detail'>
-                                    {/* ahmad */}
+                                    <center><span ><TbLockAccess className= 'circle' style={{fontSize: 30,marginTop: 20}}/></span></center>
+                                    <center><h4 className='mt-3 details'>Protect Your Data</h4></center> 
+                                    <center><p className='more_info'>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                                   Rerum rem soluta sit eius necessitatibus voluptate excepturi beatae ad 
+                                   eveniet sapiente impedit quae modi quo provident odit molestias! Rem reprehenderit assumenda</p></center>
                                 </div>
                                 <div className='detail'>
-                                    {/* ahmad */}
+                                    <center><span ><SlSupport className= 'circle' style={{fontSize: 30,marginTop: 20}}/></span></center>
+                                    <center><h4 className='mt-3 details'>24/7 Help Support</h4></center> 
+                                    <center><p className='more_info'>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                                   Rerum rem soluta sit eius necessitatibus voluptate excepturi beatae ad 
+                                   eveniet sapiente impedit quae modi quo provident odit molestias! Rem reprehenderit assumenda</p></center>
+                                </div>
+                            </div>
+                        </center>
+                    </Col>
+                    <Col md={1}></Col>
+                </Row>
+            </div>
+            <div style={{height: '550px',marginTop: "10%"}}>
+                <Row className='m-0 p-0'>
+                    {/* <Col md={1}></Col> */}
+                    <Col md={5}>
+                        <img src={require('./Happy woman chatting with friends online.jpg')} alt='Our Services' width={780} />
+                    </Col>
+                    <Col md={5}>
+                        <center>
+                            <div style={{marginTop: '40%',marginLeft: "30%",width: '100%'}}>
+                                <h1>About Us</h1>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                                   Rerum rem soluta sit eius necessitatibus voluptate excepturi beatae ad 
+                                   eveniet sapiente impedit quae modi quo provident odit molestias! Rem reprehenderit assumenda
+                                </p>
+                            </div>
+                        </center>
+                    </Col>
+                    {/* <Col md={1}></Col> */}
+                </Row>
+            </div>
+            <div style={{marginTop: "10%",backgroundColor: '#f2f2f2'}}>
+                <Row className='m-0 p-0'>
+                    <Col md={1}></Col>
+                    <Col md={10}>
+                        <center>
+                            <div>
+                                <h4 style={{fontSize: '15px',marginTop: 20}}>TESTIMONIAL</h4>
+                                <h1 className=''> Happy Costomer </h1>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                                   Rerum rem soluta sit eius necessitatibus voluptate excepturi beatae ad 
+                                   eveniet sapiente impedit quae modi quo provident odit molestias! Rem reprehenderit assumenda
+                                </p>
+                            </div>
+                            <div style={{ display: 'flex',flexDirection: 'row',paddingLeft: 140 }}>
+                                <div className='qoutes'>
+                                    <center><p className='qoutation_mark'> " </p></center>
+                                    <p>
+                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                                   Rerum rem soluta sit eius necessitatibus voluptate excepturi beatae ad 
+                                   eveniet sapiente impedit quae modi quo provident odit molestias! Rem reprehenderit assumenda
+                                    </p>
+                                    <h5 >Ahmad Kabir</h5>
+                                    <p >Hanan Pharmacetical NIG LTD</p>
+                                </div>
+                                <div className='qoutes'>
+                                    <center><p className='qoutation_mark'> " </p></center>
+                                    <p>
+                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                                   Rerum rem soluta sit eius necessitatibus voluptate excepturi beatae ad 
+                                   eveniet sapiente impedit quae modi quo provident odit molestias! Rem reprehenderit assumenda
+                                    </p>
+                                    <div> </div>
+                                    <h5 >Ahmad Kabir</h5>
+                                    <p >Hanan Pharmacetical NIG LTD</p>
                                 </div>
                             </div>
                         </center>
@@ -85,6 +161,9 @@ export default function LandingPage() {
                 </Row>
             </div>
         </main>
+        <footer>
+            Ahmad
+        </footer>
     </div>
   )
 }
